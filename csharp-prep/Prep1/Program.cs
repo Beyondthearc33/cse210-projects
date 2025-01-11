49c519784daf0739538d06649c91d5af884e62bd
Luke Lucas <lukelucas@Lukes-MacBook-Pro.local>
2025-01-11 11:44:36 -0700

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.WriteLine("What is your First Name?");
+        String firstName = Console.ReadLine();
+        Console.WriteLine(" Whatis your Last Name?");
+        String lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
