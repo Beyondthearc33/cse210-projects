f411f366bd0f76d555a241b73f1e5dc7e8cad0ea
Luke.lucas <luke.lucas001@gmail.com>
2025-02-05 20:52:46 -0700

    Fraction Draft complete

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        int top = 7;
+        int bottom = 4;
+        Fraction fraction = new Fraction(top, bottom);
+
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionString());
     }
 }
